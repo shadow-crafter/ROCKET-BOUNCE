b7fd80100a242068bc8a4f578abebbe01be0f9dc
Shadowcrafter2017 <sharkpuncher08@gmail.com>
2023-11-19 15:45:23 -0500

    added asteroids

--- scenes/Obstacles/asteroid.gd
+++ scenes/Obstacles/asteroid.gd
@@ -1,9 +1,24 @@
 class_name asteroid extends RigidBody2D
 
+@export var min_speed: float
+@export var max_speed: float
+
+@export var rotation_speed_min: float
+@export var rotation_speed_max: float
+
+var direction: float
+var rotation_speed : float
+
 @onready var sprite: Sprite2D = $Sprite
 
 func _ready() -> void:
 	sprite.frame = randi_range(0, 3)
+	rotation_speed = deg_to_rad(randi_range(rotation_speed_min, rotation_speed_max))
+	
+	linear_velocity = Vector2(randf_range(min_speed, max_speed), 0).rotated(rotation)
+
+func _process(delta: float) -> void:
+	sprite.rotate(rotation_speed * delta)
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
--- scenes/game/asteroid_spawner.gd
+++ scenes/game/asteroid_spawner.gd
@@ -3,8 +3,19 @@ extends Node2D
 @export var asteroid_scene: PackedScene
 
 @onready var spawn_timer: Timer = $SpawnTimer
-@onready var spawn_path1: Path2D = $Path1
-@onready var spawn_path2: Path2D = $Path2
+@onready var spawn_paths: Array[Path2D] = [$Path1, $Path2]
 
 func _on_spawn_timer_timeout() -> void:
-	var new_asteroid = asteroid_scene.instantiate()
+	var new_asteroid: asteroid = asteroid_scene.instantiate()
+	
+	var spawn_path: PathFollow2D = spawn_paths.pick_random().get_child(0)
+	
+	spawn_path.progress_ratio = randf()
+	
+	var direction: float = (spawn_path.rotation + PI / 2) + randf_range(-PI / 4, PI / 4)
+	
+	new_asteroid.position = spawn_path.position
+	new_asteroid.rotation = direction
+	new_asteroid.direction = direction
+	
+	add_child(new_asteroid)
